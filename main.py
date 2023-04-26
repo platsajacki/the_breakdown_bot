96b8bcb0bcd6a3b16b0cf23b2789c3f845584e64
Вячеслав Менюхов <menyukhov@bk.ru>
2023-04-26 19:33:34 +0300

    Pattern

---
+++ main.py
@@ -0,0 +1,18 @@
+'''The Breakdown Bot'''
+
+from dataclasses import dataclass
+from typing import ClassVar
+
+
+@dataclass
+class Position:
+    '''The base class of positions'''
+    ticket: str
+    stop: float
+    take_profit: float
+    LUFT: ClassVar[float] = 0.25
+
+    def count_entry_point(self) -> float:
+        raise NotImplementedError('The method in the class '
+                                  f'"{self.__class__.__name__}" '
+                                  'is not defined.')
