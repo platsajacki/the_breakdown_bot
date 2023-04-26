573f8374917018754e9e2aa0b568a5b430a3f461
Вячеслав Менюхов <menyukhov@bk.ru>
2023-04-26 22:53:40 +0300

    Add classes "Long" and "Short"

--- main.py
+++ main.py
@@ -8,13 +8,35 @@ from typing import ClassVar
 class Position:
     '''The base class of positions'''
     ticket: str
-    stop: float
-    take_profit: float
-    LUFT: ClassVar[float] = 0.25
+    level: float
+    COEF_STOP: ClassVar[float] = 0.01
+    COEF_LUFT: ClassVar[float] = 0.25
+    COEF_PROFIT: ClassVar[float] = 4
 
+    def calculate_stop(self) -> float:
+        calculatet_stop = self.level * self.COEF_STOP
+        return calculatet_stop
+
+    def calculate_luft(self) -> float:
+        luft = self.calculate_stop() * self.COEF_LUFT
+        return luft
+
+
+class Long(Position):
+    '''Calculation of a long position.'''
     def count_entry_point(self) -> float:
-        raise NotImplementedError('The method in the class '
-                                  f'"{self.__class__.__name__}" '
-                                  'is not defined.')
+        entry_point = self.level + super().calculate_luft()
+        stop = entry_point - super().calculate_stop()
+        take_profit = entry_point + self.COEF_PROFIT * super().calculate_stop()
+        print(super().calculate_stop(), super().calculate_luft(),
+              entry_point, stop, take_profit)
 
-# 1
+
+class Short(Position):
+    '''Calculation of a long position.'''
+    def count_entry_point(self) -> float:
+        entry_point = self.level - super().calculate_luft()
+        stop = entry_point + super().calculate_stop()
+        take_profit = entry_point - self.COEF_PROFIT * super().calculate_stop()
+        print(super().calculate_stop(), super().calculate_luft(),
+              entry_point, stop, take_profit)
