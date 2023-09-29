02dddf79662ee2ad78e7e711cdd742a064ebcaf9
Вячеслав Менюхов <menyukhov@bk.ru>
2023-09-29 10:22:48 +0300

    Fix bug Market.open_pos.

--- trade/bot_request.py
+++ trade/bot_request.py
@@ -45,14 +45,14 @@ class Market:
         return mark_price
 
     @staticmethod
-    def open_pos(symbol: str, entry_point: float, stop: float,
+    def open_pos(ticker: str, entry_point: float, stop: float,
                  take_profit: float, trigger: float, side: str) -> None:
         """Round the position parameters and open it."""
         # Calculation of transaction volume
         min_order_qty: str = (
             session_http.get_instruments_info(
                 category=LINEAR,
-                symbol=symbol)
+                symbol=(symbol := f'{ticker}{USDT}'))
             ['result']['list'][0]
             ['lotSizeFilter']['minOrderQty']
         )
