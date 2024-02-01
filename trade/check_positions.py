3124c828b792f90f349c78f8003ba2fe46729763
Вячеслав Менюхов <menyukhov@bk.ru>
2024-02-01 17:52:08 +0300

    Add trade.check_positions async.

--- trade/check_positions.py
+++ trade/check_positions.py
@@ -25,27 +25,32 @@ from trade.requests import Market
 config.dictConfig(LOG_CONFIG)
 logger = logging.getLogger(__name__)
 
-# Setup a connection WebSocket.
-try:
-    session_privat = WebSocket(testnet=TESTNET, api_key=API_KEY, api_secret=API_SECRET, channel_type='private')
-except Exception as error:
-    log_and_send_error(logger, error, '`WebSocket session_privat`')
+
+async def get_ws_session_privat() -> WebSocket:
+    """Setup a connection WebSocket."""
+    try:
+        ws_session_privat = WebSocket(testnet=TESTNET, api_key=API_KEY, api_secret=API_SECRET, channel_type='private')
+        ws_session_privat.ping_interval = CUSTOM_PING_INTERVAL
+        ws_session_privat.ping_timeout = CUSTOM_PING_TIMEOUT
+        return ws_session_privat
+    except Exception as error:
+        await log_and_send_error(logger, error, '`WebSocket session_privat`')
 
 
-def handle_message(msg: dict[str, Any]) -> None:
+async def handle_message(msg: dict[str, Any]) -> None:
     """The handler of messages about completed transactions. Check the trailing stop, if there is none, set."""
     for trade in msg['data']:
-        exec_time: int = int(trade['execTime'])
+        exec_time = int(trade['execTime'])
         now_in_milliseconds: int = round(time() * 1000)
         if (
             now_in_milliseconds - exec_time < MINUTE_IN_MILLISECONDS
             and trade['category'] == LINEAR
         ):
-            send_message(f'Conducted trade {InfoMessage.TRADE_MESSAGE.format(**trade)}')
+            await send_message(f'Conducted trade {InfoMessage.TRADE_MESSAGE.format(**trade)}')
             symbol: str = trade['symbol']
-            position_list: list[dict[str, Any]] | None = Market.get_open_positions(ticker=symbol[:-4])
+            position_list: list[dict[str, Any]] | None = await Market.get_open_positions(ticker=symbol[:-4])
             if position_list is None:
-                send_message('The position is completely closed.')
+                await send_message('The position is completely closed.')
                 continue
             position: dict[str, Any] = position_list[0]
             if (
@@ -63,14 +68,13 @@ def handle_message(msg: dict[str, Any]) -> None:
                     trailing_stop, active_price = Long.get_trailing_stop_param(avg_price, round_price)
                 else:
                     trailing_stop, active_price = Short.get_trailing_stop_param(avg_price, round_price)
-                Market.set_trailing_stop(
+                await Market.set_trailing_stop(
                     symbol, str(trailing_stop), str(active_price)
                 )
                 position['trailingStop'] = trailing_stop
-            send_message(f'Total position {InfoMessage.POSITION_MESSAGE.format(**position)}')
+            await send_message(f'Total position {InfoMessage.POSITION_MESSAGE.format(**position)}')
 
 
-def start_execution_stream() -> None:
-    session_privat.execution_stream(callback=handle_message)
-    session_privat.ping_interval = CUSTOM_PING_INTERVAL
-    session_privat.ping_timeout = CUSTOM_PING_TIMEOUT
+async def start_execution_stream() -> None:
+    """Start ws_session_privat.execution_stream."""
+    (await get_ws_session_privat()).execution_stream(callback=handle_message)
